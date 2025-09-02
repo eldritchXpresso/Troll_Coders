4123f21ce415cc80aa5d8b3e771f0049f701f829
eldritchXpresso <145627706+eldritchXpresso@users.noreply.github.com>
2025-09-02 09:28:18 -0400

    Add files via upload

---
+++ Program2.java
@@ -0,0 +1,33 @@
+/**
+ * Program Name: Program 2
+ * Program Description: to determine the brand of tobacco based on the
+ * 	properties of a sample of tobacco ash.
+ * Course: CS 1131
+ * Lab Section: L03
+ * Lab Team Name: Troll Coders
+ * @author Thomas Anderson, Jack Lillywhite, William Rauch, Scarlett Winter
+ */
+
+public class Program2 {
+
+	public static String classifyTobaccoAsh( String texture, String color,
+	boolean has_particles, int nicotineCount ) {
+		
+		if ( texture.equals("caked") ) {
+			
+		}
+		else if () {
+
+		}	
+		else {
+			System.out.println("INVALID DATA: UNABLE TO CLASSIFY " +
+					"TOBACCO");
+			return "INVALID_DATA";
+		}
+	}
+
+	public static void main( String[] args ) {
+		//code goes here
+	}
+
+}
