4f142eb6cdfa381876a2a3292f06dca3b1fce8e3
ThomasA323 <tbandersen2007@gmail.com>
2025-09-09 13:55:05 -0400

    Add files via upload

--- Lab3.java
+++ Lab3.java
@@ -39,14 +39,22 @@ class Lab3{
 		return exponent;
 	}
 	public static void printPowers( int base, int maxExponent ){
-		//todo
+	System.out.println("Base Exponent Power");
+		for (int i = 0; i <=maxExponent; i++) {
+		System.out.printf("%4s %8s %5s%n",base, i, power(base,i));
+	}
 	}
 	public static void printLogs( int base, int maxArgument ){
-		//todo
+		System.out.println("Base Argument Exponent");
+		for(int i = 1; i <= maxArgument;multiply(i,2) ) {
+			System.out.printf("%4s %8s %5s%n", base, log(base,i), i);
+	}
 	}
 	public static void main(String[] args){
 		System.out.println( "multiply( 4, 5 ) is " + multiply( 4, 5 ) );
 		System.out.println( "power( 4, 3 ) is " + power( 4, 3 ) );
 		System.out.println( "log( 2, 8 ) is " + log( 2, 8 ) );
+	        printPowers( 2,10 );
+		printLogs(2,1024);
 	}
 }
