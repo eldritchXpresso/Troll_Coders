9f683abb77a186caa8bbf2bb7431eb52d9b9f221
ThomasA323 <tbandersen2007@gmail.com>
2025-10-07 09:37:02 -0400

    Update MathLibraryTest.java

--- MathLibraryTest.java
+++ MathLibraryTest.java
@@ -1,34 +1,26 @@
-??? from here until ???END lines may have been inserted/deleted
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert*;
 public class MathLibraryTest {
-private static final double DELTA = 0.0001;
-@Test
-public void testPositiveValue() {
-assertEquals(5.0, MathLibrary.absoluteValue(5.0), DELTA);
-}
-@Test
-public void testNegativeValue() {
-// This test case specifies the behavior for a negative
-input
-// It should fail with the current implementation.
-assertEquals(10.0, MathLibrary.absoluteValue(-10.0), DELTA);
-}
-// Add a test case for zero (0.0) here.
-}
-@Test
-public void roundToNearestInteger_PosDecimal (double x) {
-assertEquals(7,MathLibrary.roundtoNearestIntger(6.9));
-}
-@Test
-public void roundtoNearestInteger_NegDecimal(double x) {
-	assertEquals(7, MathLibrary.roundtoNearestIntger(-6.9));
-}
-public void roundtoNearestInteger_Halves(double x) {
-	assertEquals(7, MathLibrary.roundtoNearestInteger(7.5));
-	assertEquals(-7,MathLibrary.roundtoNearestInteger(-7.5));
-}
-public void roundtoNearestInteger_WholeNumbers(double x) {
-	assertEquals(7,MathLibrary.roundtoNearestInteger(7.0));
-	assertEquals(-7,MathLibrary.roundtoNearestInteger(-7.0));
+
+    @Test
+    public void roundToNearestInteger_PosDecimal() {
+        assertEquals(7, MathLibrary.roundToNearestInteger(6.9));
+    }
+
+    @Test
+    public void roundToNearestInteger_NegDecimal() {
+        assertEquals(-7, MathLibrary.roundToNearestInteger(-6.9));
+    }
+
+    @Test
+    public void roundToNearestInteger_Halves() {
+        assertEquals(7, MathLibrary.roundToNearestInteger(7.5));
+        assertEquals(-7, MathLibrary.roundToNearestInteger(-7.5));
+    }
+
+    @Test
+    public void roundToNearestInteger_Wholes() {
+        assertEquals(7, MathLibrary.roundToNearestInteger(7.0));
+        assertEquals(-7, MathLibrary.roundToNearestInteger(-7.0));
+    }
 }
