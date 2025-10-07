7d13220cbb38a641ac71af90319892b11010fdf1
DRAGON014 <gamerboi5280@gmail.com>
2025-10-07 09:50:25 -0400

    Add files via upload

--- MathLibraryTest.java
+++ MathLibraryTest.java
@@ -1,34 +1,43 @@
-import org.junit.Test; 
+import org.junit.Test;
 import static org.junit.Assert.*;
-
 public class MathLibraryTest {
-
 	private static final double DELTA = 0.0001;
-
 	@Test
-	public void testPositiveValue( ) {
-		assertEquals( 5.0, MathLibrary.absoluteValue( 5.0 ), DELTA );
+	public void testPositiveValue() {
+		assertEquals(5.0, MathLibrary.absoluteValue(5.0), DELTA);
 	}
-
 	@Test
-	public void testNegativeValue( ) {
-		// this test case specifies the behavior for a negative
-		// input. it should fail with the current implementation.
-		assertEquals( 10.0, MathLibrary.absoluteValue( -10.0 ),
-				DELTA );
+	public void testNegativeValue() {
+		assertEquals(10.0, MathLibrary.absoluteValue(-10.0), DELTA);
 	}
+	@Test
+	public void testPowerOfTwo() {
+		assertEquals(0, MathLibrary.powerOfTwo(0), DELTA);
+	}
+	@Test
+	(expected = IllegalArgumentException.class)public void testNegativePowerOfTwo() {
+		MathLibrary.powerOfTwo(-3);
+	}
+	@Test
+	public void roundToNearestInteger_PosDecimal() {
+        	assertEquals(7, MathLibrary.roundToNearestInteger(6.9), DELTA);
+    	}
 
-	// add a test case for zero ( 0.0 ) here.
+    	@Test
+    	public void roundToNearestInteger_NegDecimal() {
+        	assertEquals(-7, MathLibrary.roundToNearestInteger(-6.9), DELTA);
+    	}
+
+    	@Test
+    	public void roundToNearestInteger_Halves() {
+        	assertEquals(8, MathLibrary.roundToNearestInteger(7.5), DELTA);
+        	assertEquals(-7, MathLibrary.roundToNearestInteger(-7.5), DELTA);
+    	}
+
+    	@Test
+    	public void roundToNearestInteger_Wholes() {
+        	assertEquals(7, MathLibrary.roundToNearestInteger(7.0), DELTA);
+        	assertEquals(-7, MathLibrary.roundToNearestInteger(-7.0), DELTA);
+    	}
 	
-	// tests for MathLibrary.truncate( double x )
-	@Test
-	public void testTruncatePositiveValue( ) {
-		assertEquals( 4.0, MathLibrary.truncate( 4.99 ), DELTA );
-	}
-
-	@Test
-	public void testTruncateInvalidInput( ) {
-		assertThrows( IllegalArgumentException.class, 
-				MathLibrary.truncate( Double.NaN ), DELTA );
-	}
 }
