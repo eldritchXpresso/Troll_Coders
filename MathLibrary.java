012e41006f95dd93b0ffce6d081f04ebcf71d171
ThomasA323 <tbandersen2007@gmail.com>
2025-10-07 09:17:17 -0400

    Update MathLibrary.java

--- MathLibrary.java
+++ MathLibrary.java
@@ -18,3 +18,21 @@ public class MathLibrary
 		}
 	}
 }
+public static double roundtoNearestInteger (double x) {
+        int whole = (int) x;
+        double fraction = x - whole;
+
+        if (x >= 0) {
+                if (fraction >= .5) {
+                        return whole + 1;
+                } else {
+                        return whole;
+                }
+        } else {
+        if( fraction <= -.5) { 
+                return whole;
+        } else {
+        return whole -1;
+        } 
+        }
+}
