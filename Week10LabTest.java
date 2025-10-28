694a0007eb4ae6ef9f02d87ecbec6d8991d9448f
ThomasA323 <tbandersen2007@gmail.com>
2025-10-28 09:29:44 -0400

    Update Week10LabTest.java

--- Week10LabTest.java
+++ Week10LabTest.java
@@ -3,11 +3,29 @@ import org.junit.Test;
 
  /**
   * Tests cases
-  * @Jack Lillywhite
+  * @Jack Lillywhite, Thomas Andersen, William Rauch, Scarlett Winter
   * */
 
  public class Week10LabTest 
 {
+        @Test
+public void isPalindrome() {
+Week10Lab kevin = new Week10Lab();
+assertTrue(kevin.isPalindrome(""));
+
+assertTrue(kevin.isPalindrome("t"));
+
+assertTrue(kevin.isPalindrome("kayak"));
+
+assertFalse(kevin.isPalindrome("Patriots"));
+
+}
+
+/**
+ * The tests that will run are going to be an empty string, an one charcter string, a palindrome, not a palindrome.
+ */
+}
+
 	@Test
 	public void sumDigits()
 	{
